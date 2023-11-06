acbf1e3e2d7930dc840dacb2dcd72188121d5e2d
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-11-07 02:17:15 +0530

    adding footer builder

--- lib/auth_Gate.dart
+++ lib/auth_Gate.dart
@@ -34,6 +34,16 @@ class AuthGate extends StatelessWidget {
                    : const Text('Welcome to Flutterfire, please sign up!'),
              );
            },
+           footerBuilder: (context, action) {
+             return const Padding(
+               padding: EdgeInsets.only(top: 16),
+               child: Text(
+                 'By signing in, you agree to our terms and conditions.',
+                 style: TextStyle(color: Colors.grey),
+               ),
+             );
+           },
+           
           );
         }
 
