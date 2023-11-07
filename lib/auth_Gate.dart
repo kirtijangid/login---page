61582b4dd4e4a41f00bf60334dc873f0ffba7f19
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-11-07 22:25:02 +0530

    adding icon to homepage

--- lib/auth_Gate.dart
+++ lib/auth_Gate.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:login_page/home.dart';
 
 
 
@@ -43,7 +44,12 @@ class AuthGate extends StatelessWidget {
                ),
              );
            },
-           
+           sideBuilder: (context, shrinkOffset) {
+             return Padding(padding: EdgeInsets.all(20),
+             child: AspectRatio(aspectRatio: 1,
+             child: Image.asset('asset/spotify.png'),),
+             );
+           },
           );
         }
 
@@ -53,11 +59,3 @@ class AuthGate extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
---
+++ lib/home.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+   HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<ProfileScreen>(
+                  builder: (context) =>  ProfileScreen(
+                    actions: [
+                      SignedOutAction((context) {
+                        Navigator.of(context).pop();
+                      }),
+                    ],
+                  ),
+                ),
+              );
+            },
+          )
+        ],
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/spotify.png'),
+            Text(
+              'Welcome!',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          const SignOutButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+    
